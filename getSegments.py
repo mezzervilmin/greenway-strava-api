ee3805f36e6e8e54016727e8c40c8dcd4659f002
Adam Vilmin <mezzervilmin@gmail.com>
2025-01-15 19:47:25 -0500

    fixed bug in file writing

--- getSegments.py
+++ getSegments.py
@@ -5,13 +5,13 @@ write_file = open("segments.json", "a")
 
 with open('segmentsToCheck.json') as f:
     segments = json.load(f)['segments']
-    headers =  {"Content-Type": "application/json", "Authorization": "Bearer 856afca7900aa8e703abd0221c1cb5c2a291001c"}
+    headers =  {"Content-Type": "application/json", "Authorization": "Bearer 61967b1c4c076129d3f2e276058a02c047139603"}
     url = "https://www.strava.com/api/v3/segments/{id}"
     write_data = {"segments": []}
     
     for segment in segments:
-        url = url.format(id=segment['id'])
-        r = requests.get(url=url, headers=headers)
+        request_url = url.format(id=segment['id'])
+        r = requests.get(url=request_url, headers=headers)
         write_data["segments"].append(r.json())
     json.dump(write_data, write_file)
 
